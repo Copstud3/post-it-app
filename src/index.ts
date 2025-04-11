ba1630a7f8934286e545820ad7ccdc1feca74fe0
Chris <victorchris73@gmail.com>
2025-04-11 23:54:59 +0100

    feat: add CORS middleware for Render deployment

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,18 @@ import userRoutes from "./routes/userRoutes";
 import postRoutes from "./routes/postRoutes";
 import commentRoutes from "./routes/commentRoutes";
 import { setupSwagger } from "./swagger";
+import cors from "cors"; 
 
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Add CORS middleware
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
 app.use(express.json());
 app.use("/users", userRoutes);
