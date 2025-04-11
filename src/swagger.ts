4d1b2a12388e3e6db5fdafabb6c1ad666d33107e
Chris <victorchris73@gmail.com>
2025-04-11 14:17:37 +0100

    feat: move Swagger docs to /api/v1/docs

--- src/swagger.ts
+++ src/swagger.ts
@@ -23,6 +23,6 @@ const options: swaggerJsdoc.Options = {
 const swaggerSpec = swaggerJsdoc(options);
 
 export const setupSwagger = (app: Express) => {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-  console.log("Swagger docs available at http://localhost:3000/api-docs");
-};
+    app.use("/api/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+    console.log("Swagger docs available at /api/v1/docs");
+  };
